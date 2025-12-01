658341754c009a7af6f4f94abd88aae1eb3d463a
ashleymararo <ashleymararo222@gmail.com>
2025-12-01 14:46:28 +0300

    finished lab

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,25 @@
 class MySet:
 
-    pass
+
+    def __init__(self, iterable=None):
+        """Initialize the set with unique elements from the iterable, if given."""
+        self.dictionary = {}
+        if iterable:
+            for item in iterable:
+                self.dictionary[item] = True
+
+    def add(self, item):
+        """Add an element to the set."""
+        self.dictionary[item] = True
+
+    def delete(self, item):
+        """Remove an element from the set, if it exists."""
+        if item in self.dictionary:
+            del self.dictionary[item]
+
+    def has(self, item):
+        """Check if an element exists in the set."""
+        return item in self.dictionary
+
+    def __repr__(self):
+        return f"MySet({list(self.dictionary.keys())})"
